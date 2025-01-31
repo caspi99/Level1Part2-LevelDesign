7b3d2d1ffcfe9f80c45a52c6a3b408d2c736b831
CCCarluchi <carlacolladocrespo@gmail.com>
2025-01-31 12:48:21 +0100

    Elevator platform fix

--- Level1Part2-LevelDesign/Assets/scripts/PlatformTrigger.cs
+++ Level1Part2-LevelDesign/Assets/scripts/PlatformTrigger.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 public class PlatformTrigger : MonoBehaviour
 {
     public Animator platformAnimator;
+    public Transform platform;
     public Transform player;
-
+    private bool inPlatform = false;
+    public CharacterController controller;
     private Vector3 lastPlatformPosition;
+    public float var = 2f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            inPlatform = true;
             platformAnimator.SetTrigger("Activate");
-            lastPlatformPosition = transform.position;
+            lastPlatformPosition = platform.position;
         }
     }
 
@@ -20,18 +24,22 @@ public class PlatformTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            player = null;
+            //player = null;
+            inPlatform = false;
         }
     }
 
     private void Update()
     {
-        if (player != null)
+        if (inPlatform)
         {
-            Vector3 platformMovement = transform.position - lastPlatformPosition;
+            controller.enabled = false;
+            Vector3 platformMovement = (platform.position - lastPlatformPosition)*var;
             player.position += platformMovement; // Move the player with the platform
+            controller.enabled = true;
+            Debug.Log(player.position);
             Debug.Log(platformMovement.x);
-            lastPlatformPosition = transform.position;
+            lastPlatformPosition = platform.position;
         }
     }
 }
