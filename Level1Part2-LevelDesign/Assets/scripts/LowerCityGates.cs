f6f36cba29d042519eb7ff0b356f8e9ae4faab87
Joan Llobet <j.llobetruiz@gmail.com>
2025-02-01 18:30:14 +0100

    ferdiad city gates trigger

--- Level1Part2-LevelDesign/Assets/scripts/LowerCityGates.cs
+++ Level1Part2-LevelDesign/Assets/scripts/LowerCityGates.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LowerCityGates : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    
+    public Transform cityGates;
+    private float y;
 
     void Start()
     {
-        
+        y = transform.position.y;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            cityGates.position = new Vector3(cityGates.position.x, cityGates.position.y - 20, cityGates.position.z);
+            gameObject.SetActive(false);
+        }
     }
+
+
 }
